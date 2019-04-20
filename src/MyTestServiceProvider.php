780e7c269e5c1ab07fa22ed101dc749563e48202
phu <vongocphu04@gmail.com>
2019-04-20 18:19:24 +0700

    update model view

--- src/MyTestServiceProvider.php
+++ src/MyTestServiceProvider.php
@@ -6,7 +6,7 @@
         public function boot()
         {
             $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-            // $this->loadViewsFrom(__DIR__.'/resources/views', 'contactform');
+            $this->loadViewsFrom(__DIR__.'/views', 'nhoma');
             // $this->loadMigrationsFrom(__DIR__.'/Database/migrations');
         }
         public function register()
--- src/routes/web.php
+++ src/routes/web.php
@@ -3,3 +3,16 @@
 Route::get('/hello',function(){
     echo "hello";
 });
+
+Route::group(['namespace'=>'Phuvo\Apinhoma\Controllers','prefix'=>'api'],function(){
+    Route::get('/users','UserController@index')->name('showalluser');
+    Route::post('/users/page','UserController@paging')->name('paging');
+    Route::get('/users/search','UserController@search')->name('search');
+    Route::get('/users/{id}','UserController@showUser')->name('showauser');
+    Route::post('/users/login','UserController@loginUser')->name('loginUser');
+    Route::post('/users/logout','UserController@logoutUser')->name('logoutUser');
+    Route::post('/users/create','UserController@createUser')->name('createUser');
+    Route::put('/users/update/{id}','UserController@updateUser')->name('updateuser');
+    Route::put('/users/change-password/{id}','UserController@changeUserPassword')->name('change-password');
+    Route::delete('/users/delete/{id}','UserController@deleteUser')->name('deleteuser');
+});
